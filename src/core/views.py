e7e37bbf269562900d2e47323802dc5880afb34c
mattfreire <freire1996@gmail.com>
2019-04-04 11:01:59 +0200

    Can now filter by category and reviewed

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,6 @@
-from django.db.models import Q
-from django.shortcuts import render
-from .models import Journal
+from django.db.models import Q, Count
+from django.shortcuts import render, get_object_or_404
+from .models import Journal, Category
 
 
 def is_valid_queryparam(param):
@@ -9,6 +9,7 @@ def is_valid_queryparam(param):
 
 def BootstrapFilterView(request):
     qs = Journal.objects.all()
+    categories = Category.objects.all()
     title_contains_query = request.GET.get('title_contains')
     id_exact_query = request.GET.get('id_exact')
     title_or_author_query = request.GET.get('title_or_author')
@@ -16,6 +17,9 @@ def BootstrapFilterView(request):
     view_count_max = request.GET.get('view_count_max')
     date_min = request.GET.get('date_min')
     date_max = request.GET.get('date_max')
+    category = request.GET.get('category')
+    reviewed = request.GET.get('reviewed')
+    not_reviewed = request.GET.get('notReviewed')
 
     if is_valid_queryparam(title_contains_query):
         qs = qs.filter(title__icontains=title_contains_query)
@@ -40,7 +44,17 @@ def BootstrapFilterView(request):
     if is_valid_queryparam(date_max):
         qs = qs.filter(publish_date__lt=date_max)
 
+    if is_valid_queryparam(category) and category != 'Choose...':
+        qs = qs.filter(categories__name=category)
+
+    if reviewed == 'on':
+        qs = qs.filter(reviewed=True)
+
+    elif not_reviewed == 'on':
+        qs = qs.filter(reviewed=False)
+
     context = {
-        'queryset': qs
+        'queryset': qs,
+        'categories': categories
     }
     return render(request, "bootstrap_form.html", context)
