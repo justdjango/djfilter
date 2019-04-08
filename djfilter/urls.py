6fdbf72acec763380a368460a9d436f26451356f
mattfreire <freire1996@gmail.com>
2019-04-08 08:36:00 +0200

    React project  configured

--- djfilter/urls.py
+++ djfilter/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from core.views import BootstrapFilterView
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
-    path('', BootstrapFilterView, name='bootstrap')
+    path('', BootstrapFilterView, name='bootstrap'),
+    re_path(r'^react/', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
